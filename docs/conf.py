aeae808ea66dea80254dfd6aff28a1c93264cba2
John Veillette <johnv@uchicago.edu>
2024-07-24 15:48:28 -0500

    iterate version

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'p2prev'
 copyright = '2024, John Veillette'
 author = 'John Veillette'
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
